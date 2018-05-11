f6358c9729c6476ae0781c07dbf2856f04b5b953
Chester Pang <i@bopa.ng>
2018-05-12 05:49:03 +1000

    Added route for post

--- routes.py
+++ routes.py
@@ -7,13 +7,6 @@ from utils import *
 def discount_feed():
     return render_template("index.html")
 
-@app.route('/discount_post')
-def discount_post():
-    if request.method == 'POST':
-        PostManager.add_post(dict(request.form))
-        return redirect(url_for('dashboard.html'))
-    return render_template("discount_post.html")
-
 @app.route('/feed')
 def feed():
     post_list = PostManager.get_all_posts()
@@ -24,7 +17,12 @@ def dashboard():
     categories = []
     return render_template("dashboard.html", categories=categories)
 
-
+@app.route('/post')
+def post():
+    if request.method == 'POST':
+        PostManager.add_post(dict(request.form))
+        return redirect(url_for('dashboard'))
+    return render_template('post.html')
 
 '''
 Serve static files
