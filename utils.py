65adf99445526c6e6a8ee8b481a4afd5529b45d8
XifeiNi <31680997+XifeiNi@users.noreply.github.com>
2018-05-12 06:36:53 +1000

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,61 @@
+from models import *
+from server import dao
+from config import *
+
+class UserManager:
+    def add_user(user_form):
+        # count curr user number
+        uid = len(dao.read(USERS)) + 1
+        user_form['id'] = uid
+        dao.write(USERS, [user_form])
+
+    def get_user_by_id(id):
+        user_dict = next(filter(lambda u:int(u['id']) == id, dao.read(USERS)))
+        return User(user_dict)
+
+    def get_users():
+        return [ User(user_dict) for user_dict in dao.read(USERS) ]
+
+
+class PostManager:
+    def add_post(data_dict):
+        all_posts = dao.read(POSTS)
+        data_dict['id'] = len(all_posts)
+        dao.write(POSTS, [data_dict], mode="a")
+
+
+    def remove_post(post):
+        data = dao.read(POSTS)
+        data = [row for row in data if int(row['id']) != post.id ]
+        dao.write(POSTS, data, mode="w")
+
+    def name_search(name):
+        data = dao.read(POSTS)
+        data = [row for row in data if name in row['name']]
+        data = [Discount_post(row) for row in data]
+        return data
+
+    def category_search(category):
+        data = dao.read(POSTS)
+        data = [row for row in data if name in row['category']]
+        data = [Discount_post(row) for row in data]
+        return data
+
+    def get_all_posts():
+        data = dao.read(POSTS)
+        data = [Discount_post(row) for row in data]
+        return data
+
+    def get_post_by_user(id):
+        data = dao.read(USER_POSTS)
+        data = [Discount_post(row) for row in data if USER_POSTS['uid'] == id]
+        post_obj = dao.read(POSTS)
+        post_obj = [Discount_post(row) for row in post_obj]
+        post = []
+        for post_id in data:
+            for obj in post_obj:
+                if POSTS['id'] == post_id:
+                    post.append(obj)
+        return post
+
+
