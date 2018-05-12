84e4a18114c905ff32ff9881a061811e0b511eaf
Deepanjan <deepanjan.chakrabarty@student.unsw.edu.au>
2018-05-12 12:15:09 +1000

    Add facebook api code

--- server.py
+++ server.py
@@ -7,5 +7,5 @@ app.config['SECRET_KEY'] = "a"
 dao = csvDAO(app.root_path)
 UPLOAD_FOLDER = 'static/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+graph = facebook.GraphAPI(access_token=config.fb_key, version="3.0")
 
